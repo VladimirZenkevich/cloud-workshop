b510993a98e4b2d98d0202d1fcf008dfe446a66c
uladzimir.ziankevich <AckiSkeiter1!>
2015-12-14 16:22:08 +0300

    Implement Eureka server through Spring Cloud.

---
+++ eureka-server/src/main/java/com/altoros/eureka/EurekaApplication.java
@@ -0,0 +1,18 @@
+package com.altoros.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by uladzimir.ziankevich on 12/14/2015.
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+
+}
